07cc8f6588faaad4ebacfb595b7b212164343afd
csjou.hwu@gmail.com <csjou.hwu@gmail.com>
2017-11-01 18:26:18 +0800

    Add wk7 examples

---
+++ wk7/wk71.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace 命名空間1
+{
+    public class 類別1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("周重石你好!");
+        }
+    }
+}
---
+++ wk7/wk75.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace 命名空間1
+{
+    public class 類別1
+    {
+        public static void Main()
+        {
+            Random 亂數物件 = new Random();
+            for (int i = 0; i < 31; i++)
+            {
+                Console.WriteLine("座號:{0} 分數:{1}", i+1, 60 + 亂數物件.Next(36));
+            }
+        }
+    }
+}
